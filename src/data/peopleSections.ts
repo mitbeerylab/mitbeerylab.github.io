c97c0141942335dac01cf30833f9e5faa4d82b8a
Elena Sierra <esierra@stanford.edu>
2025-11-21 14:58:53 -0500

    add new people sections

---
+++ src/data/peopleSections.ts
@@ -0,0 +1,22 @@
+export type PeopleGroupEntry = { name: string; note?: string };
+
+export const mastersVisitingSection: PeopleGroupEntry[] = [
+	{ name: "Kai Van Brunt" },
+	{ name: "Quinn Perian" },
+];
+
+export const undergraduateSection: PeopleGroupEntry[] = [
+	{ name: "Wendy Cao" },
+	{ name: "Serena Pei" },
+	{ name: "Evelyn Zhu" },
+];
+
+export const alumniSection: PeopleGroupEntry[] = [
+	{ name: "Yuyan Chen", note: "visited summer 2024 from McGill" },
+	{ name: "Xenia Zhao", note: "Undergraduate" },
+	{ name: "Asmi Kumar", note: "Undergraduate" },
+	{ name: "Ada Tsui", note: "Undergraduate" },
+	{ name: "David Fang", note: "Undergraduate" },
+	{ name: "Ari Pero", note: "Undergraduate" },
+	{ name: "Avi Sundaresan", note: "Undergraduate from Caltech" },
+];
