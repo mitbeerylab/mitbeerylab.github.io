44e22408fadc38185f04fa898f9574187bc875d2
Elena Sierra <esierra@stanford.edu>
2026-03-24 16:10:06 -0400

    fit syntax error

--- src/data/peopleSections.ts
+++ src/data/peopleSections.ts
@@ -12,7 +12,7 @@ export const undergraduateSection: PeopleGroupEntry[] = [
 ];
 
 export const alumniSection: PeopleGroupEntry[] = [
-	{ name: "Edward Vendrow", note: "PhD Student"}
+	{ name: "Edward Vendrow", note: "PhD Student"},
 	{ name: "Tian Xie", note: "Undergraduate visited Summer 2025 from Harvey Mudd" },
 	{ name: "Yuyan Chen", note: "PhD student visited summer 2024 from McGill" },
 	{ name: "Xenia Zhao", note: "Undergraduate" },
